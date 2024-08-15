bfde88d6c018fd101b7ce9df85909413e60582ec
MartasinH <lugom15122@gmail.com>
2024-08-15 15:36:14 -0600

    primer commit

---
+++ curriculum/scripts/script.js
@@ -0,0 +1,3 @@
+document.getElementById("contactButton").addEventListener("click", function() {
+    window.location.href = "mailto:lugom15122@gmail.com?subject=Contacto%20Profesional&body=Hola%20Marta%20Lugo,%0A%0AEstoy%20interesado%20en%20contactarte%20por%20motivos%20profesionales.%0A%0A¡Gracias :))!";
+});
